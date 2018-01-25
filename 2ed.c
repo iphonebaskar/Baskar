0197f33e684a1438523baf68cbe3a4adf0bf256d
iphonebaskar <35652302+iphonebaskar@users.noreply.github.com>
2018-01-25 16:25:11 +0530

    Create 2ed.c

---
+++ 2ed.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int a;
+printf("Enter a number");
+scanf("%d",&a);
+if (a/2)
+{
+printf("The number is even");
+}
+else
+{
+printf("The number is odd");
+}
+getch();
+}
