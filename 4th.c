d07a44e904e43ec4588377274e8433c8c848428c
iphonebaskar <35652302+iphonebaskar@users.noreply.github.com>
2018-01-25 16:59:40 +0530

    Create 4th.c

---
+++ 4th.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char B;
+    clrscr();
+    printf("Enter\n");
+    scanf("%c",&B);
+    if((B>='a' && B<='z')||(B>='A' && G<='Z'))
+    printf("It is an Alphabet\n");
+    else
+    printf("It is not an alphabet\n");
+    getch();
+}
