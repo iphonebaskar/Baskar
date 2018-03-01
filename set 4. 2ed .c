6a969516fd457a87cdfeb5022da9e9ff591dc4bd
iphonebaskar <35652302+iphonebaskar@users.noreply.github.com>
2018-03-01 16:05:25 +0530

    Create set 4. 2ed .c

---
+++ set 4. 2ed .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+    char a[20];
+    int j,count=1,c;
+    printf("enter the string");
+    gets(a);
+    c=strlen(a);
+    if(a[j]==' ')
+    {
+        count=count+1;
+    }
+    printf("the total words is %d",count);
+getch();
+}
