8b414889298ba193e137ad0202c612506990a761
iphonebaskar <35652302+iphonebaskar@users.noreply.github.com>
2018-03-15 16:10:24 +0530

    Update set 4. 34 .c

--- set 4. 34 .c
+++ set 4. 34 .c
@@ -2,14 +2,14 @@
 #include<string.h>
 int main()
 {
-	char s[500];
-	int count=0,len,i;
-	scanf("%[^\n]s",s);
-	len=strlen(s);
-	for(i=0;i<=len;i++)
-	{
-	if(s[i]=='.')
-	count++;
-	}
-	printf("%d",count+1);
+char s[500];
+int count=0,len,i;
+scanf("%[^\n]s",s);
+len=strlen(s);
+for(i=0;i<=len;i++)
+{
+if(s[i]=='.')
+count++;
+}
+printf("%d",count+1);
 }
